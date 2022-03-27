4ea684972e15e979acc4f909641c74eca70b05d5
Joanna-1996 <Joanna.a.s.1996@Gmail.com>
2022-03-28 00:23:24 +0300

    cookies + google analytics

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -9,11 +9,7 @@
 // 		}
 // 	});
 // });
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-2PRGZZ179B');
 
 
 var myFullpage = new fullpage('#fullpage', {
